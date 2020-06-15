7ad0746b0fafd1e23422a1acb692855fb0022fb9
TTANF_1 <982677174@qq.com>
2020-06-15 20:56:39 +0800

    rem适配设置

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 引用全局样式表
 import './styles/index.less'
+
+// 导入vant组件库
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
+// 适配设置rem基准值插件
+import 'amfe-flexible'
 
 Vue.use(Vant)
 
