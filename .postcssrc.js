d772a005632492dbf8bf2c4520ea1ed647400b5b
TTANF_1 <982677174@qq.com>
2020-06-15 21:36:46 +0800

    封装请求

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,16 @@
 module.exports = {
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // vueCLI 内部已经集成了 autoprefixer 插件 注释以避免冲突
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      // rootValue: 37.5,
+      // vant 的样式就设置为37.5
+      // 自己写的样式就设置为设计稿的10分之1  因为 amfe-flexible 组件是将页面适配分成10份
+      rootValue({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
       propList: ['*']
     }
   }
